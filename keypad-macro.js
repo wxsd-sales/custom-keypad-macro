acfc7e58f989c34de0e779ad8d382290f3cc9d98
William Mills <wmpmills@gmail.com>
2022-11-18 16:16:01 +0100

    Update keypad-macro.js

--- keypad-macro.js
+++ keypad-macro.js
@@ -1,3 +1,35 @@
+
+
+/********************************************************
+Copyright (c) 2022 Cisco and/or its affiliates.
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+               https://developer.cisco.com/docs/licenses
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+*********************************************************
+ * 
+ * Macro Author:      	William Mills
+ *                    	Technical Solutions Specialist 
+ *                    	wimills@cisco.com
+ *                    	Cisco Systems
+ * 
+ * Version: 1-0-0
+ * Released: 11/18/22
+ * 
+ * This is a simple macro which creates a custom Dial icon and keypad 
+ * where you can enter a phone number and place a call.
+ * 
+ * Full Readme and source code availabel on Github:
+ * https://github.com/wxsd-sales/keypad-macro
+ * 
+ ********************************************************/
+
 import xapi from 'xapi';
 
 
